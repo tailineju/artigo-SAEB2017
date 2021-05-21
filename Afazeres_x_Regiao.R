4c9537385b347d1c2dfcb920a1524b63a25765c2
tailine <56554634+tailineju@users.noreply.github.com>
2021-05-21 00:12:08 -0300

    pacman e divisão em seções

--- Afazeres_x_Regiao.R
+++ Afazeres_x_Regiao.R
@@ -1,11 +1,25 @@
-library(tidyverse)
-#library(sqldf)
-library(dplyr)
-library(RColorBrewer)
+#Carregando pacotes ----
+
+if (!require(pacman)) {
+  install.package("pacman")
+  library(pacman)}
+
+pacman::p_load(tidyverse,dplyr,RColorBrewer)
+
+theme.t <- function(position_legend = "top"){
+  return(list(
+    theme_bw(),
+    theme(axis.title.y=element_text(colour="black", size=12),
+          axis.title.x = element_text(colour="black", size=12),
+          axis.text = element_text(colour = "black", size=9.5),
+          panel.border = element_blank(),
+          axis.line = element_line(colour = "black")),
+    theme(legend.position=position_legend)))}
+
+#Dados ----
 
 set.seed(123)
-df <- read_csv(
-  "C:\\Users\\SONY\\Desktop\\unb\\TrabalhoFinal\\amostra_180111558.csv",
+df <- read_csv("amostra_180111558.csv",
   col_names = TRUE,
   col_types = NULL,
   locale = default_locale(),
@@ -19,8 +33,10 @@ df <- read_csv(
   skip_empty_rows = TRUE
 )
 
+
 amostra <- sample_n(df, 200)
 
+#Limpeza dos dados ----
 
 amostra$REGIAO <- amostra$REGIAO%>%
   str_replace("1", "Norte")%>%
@@ -29,12 +45,11 @@ amostra$REGIAO <- amostra$REGIAO%>%
   str_replace("4", "Sul")%>%
   str_replace("5", "Centro-Oeste")
 
-
-
 amostra <- amostra %>% 
   filter(!is.na(AFAZERES_DOM)) %>%
   filter(!is.na(REGIAO))
 
+#Categorização ----
 
 A <- amostra %>%
   group_by(AFAZERES_DOM, REGIAO) %>%
@@ -80,21 +95,23 @@ total <- sum(E$Ni)
 E <- E %>%
   mutate(Fi = round((Ni/total)*100,4))
 
-
+#Teste de independência ----
 M <- as.table(rbind(A$Ni, B$Ni, C$Ni, D$Ni, E$Ni))
 dimnames(M) <- list(afazeres = c("A","B", "C","D","E"),
                     regiao =c("Centro-Oeste", "Nordeste","Norte", "Sudeste","Sul") )
 M
 (R<-chisq.test(M))
 
-# tabela de conting?ncia dos valores esperados
+#Tabela de contingência dos valores esperados ----
 ME = rbind(R$expected, 
            total=apply(R$expected,2,sum))
 ME
 
-# conclus?o que n?o h? independ?ncia entre afazeres dom?sticos e regi?es
-# h? uma associa??o entre afazeres dom?sticos e regi?o
+# conclusão que não há independência entre afazeres domésticos e regiões
+# há uma associação entre afazeres  domésticos e regiões
 
+
+#Dados completos e tratados ----
 regioes <- rbind(A, B, C, D, E)
 
 regioes$AFAZERES_DOM <- regioes$AFAZERES_DOM%>%
@@ -105,7 +122,8 @@ regioes$AFAZERES_DOM <- regioes$AFAZERES_DOM%>%
   str_replace("^E$", "Não faz")
 
 ordem_ad <- c("Menos de 1 hora", "Entre 1 e 2 horas", "Mais de 2 horas", "Mais de 3 horas", "Não faz")
-  
+
+#Análise gráfica ----
 ggplot(data=regioes,aes(x=factor(AFAZERES_DOM,levels = ordem_ad), y=Fi,fill=REGIAO)) + 
   geom_bar(stat="identity",position="stack")+
   labs(x="Tempo gasto em afazeres domésticos", y="Frequência relativa")+
